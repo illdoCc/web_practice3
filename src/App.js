8c26bad5396877311dd1a7befa381c8c54462df1
illdoCc <103tony50505@gmail.com>
2025-03-23 23:02:25 +0800

    create React by webpack

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function MyButton() {
+    return (
+      <button>
+        我是一个按钮
+      </button>
+    );
+  }
+  
+  export default function MyApp() {
+    return (
+      <div>
+        <h1>欢迎来到我的应用</h1>
+        <MyButton />
+      </div>
+    );
+  }
+  
